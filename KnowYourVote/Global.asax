87d1762433f3761d35a9d0068320045149ab5cf7
lilykrichels <lily.krichels@gmail.com>
2017-06-07 14:24:38 -0400

    First Commit

---
+++ KnowYourVote/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KnowYourVote.MvcApplication" Language="C#" %>
---
+++ KnowYourVote/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KnowYourVote.Startup))]
+namespace KnowYourVote
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
